b7482ef34a8b01fc01fde199494ba4f9d28e22da
Luca Pichon <118428580+Pichonpignon@users.noreply.github.com>
2024-11-18 09:12:14 +0100

    ajout liste de 20 mots

--- motus.php
+++ motus.php
@@ -2,6 +2,9 @@
 
 require_once 'Class/BO/MotusGame.php';
 
-$mots = ["chaise", "banane", "soleil", "livres", "cactus"];
+$mots = ["chaise", "banane", "soleil", "livres", "cactus",
+    "orange", "papaye", "valise", "fenetre", "voiture",
+    "canard", "tomate", "guitare", "nuage", "elephant",
+    "ballon", "plante", "crayon", "cuisine", "chateau"];
 $game = new MotusGame($mots);
 $game->start();
